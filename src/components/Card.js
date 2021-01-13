6ba92cabda78418fea5e9b9e6a470a004b016a83
Martin Sagastume <msr986@gmail.com>
2021-01-13 20:27:54 +0100

    homepage part 2

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,11 @@
 import styled from "styled-components"
 
 const Card = styled.div`
+  display: flex;
   box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
   transition: 0.3s;
   padding: 10px;
+  margin: 10px;
 `
 
 export default Card
--- src/sections/TechnologySection.js
+++ src/sections/TechnologySection.js
@@ -1,9 +1,38 @@
 import Card from "../components/Card";
+import Kaneki from "../assets/kaneki1.jpg";
+
+const styles = {
+    column: {
+        width: "50%",
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column"
+    }
+}
 
 const TechnologySection = () => (
     <>
         <Card>
-            Card content goes here
+            <div style={styles.column}>
+                <img src={Kaneki} style={{objectFit: "contain", maxHeight: 500}} />
+            </div>
+            <div style={styles.column}>
+                <h1>Technology</h1>
+                <ul>
+                    <li>
+                        React
+                    </li>
+                    <li>
+                        Javascript
+                    </li>
+                    <li>
+                        NodeJS
+                    </li>
+                    <li>
+                        Sales
+                    </li>
+                </ul>
+            </div>
         </Card>
     </>
 )
